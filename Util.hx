3b33b31c7f9ab87fdcec736da604aedc7482e052
idklel01 <contafalsa8322@gmail.com>
2025-10-03 16:53:16 -0300

    ata

---
+++ Util.hx
@@ -0,0 +1,63 @@
+package utils;
+
+#if openfl
+import openfl.utils.Assets;
+#end
+#if sys
+import sys.io.File;
+import sys.FileSystem;
+#end
+
+using StringTools;
+
+/**
+ * ...
+ * @author Idklool (idklel01)
+ */
+
+class Util
+{
+    public static function getContent(id:String):String
+    {
+        #if sys
+        return File.getContent(id);
+        #elseif openfl
+        return Assets.getText(id);
+        #else
+        return '';
+        #end
+    }
+
+    public static function exists(id:String):Bool
+    {
+        #if sys
+        return FileSystem.exists(id);
+        #elseif openfl
+        return Assets.exists(id);
+        #else
+        return false;
+        #end
+    }
+
+    public static function getBytes(id:String)
+    {
+        #if sys
+        return File.getBytes(id);
+        #elseif openfl
+        return Assets.getBytes(id);
+        #else
+        return null;
+        #end
+    }
+
+    public static function readDirectory(library:String):Array<String>
+    {
+        #if sys
+        return FileSystem.readDirectory(library);
+        #elseif openfl
+        return Assets.list().filter(text -> text.indexOf(library) != -1);
+        #else
+        return [];
+        #end
+    }
+}
